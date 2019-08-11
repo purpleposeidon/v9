ba6a43a83647a3a07651ed69981d30b54d7aaaf4
neptunepink <purpleposeidon@gmail.com>
2019-08-10 19:13:31 -0700

    Simplify kernel_arg test.

--- tests/kernel_arg.rs
+++ tests/kernel_arg.rs
@@ -17,49 +17,45 @@ impl<'a, 'b> Scary<'a, 'b> {
     }
 }
 
-// We can improve this by creating a wrapper!
-// It'll extract the 'static, and expose it as an arbitrary lifetime.
-struct Forcecast<'e, 'a, 'b> {
-    data: &'e mut Scary<'a, 'b>,
-}
-impl<'e, 'a, 'b> Deref for Forcecast<'e, 'a, 'b> {
-    type Target = Scary<'a, 'b>;
-    fn deref(&self) -> &Scary<'a, 'b> { self.data }
-}
-impl<'e, 'a, 'b> DerefMut for Forcecast<'e, 'a, 'b> {
-    fn deref_mut(&mut self) -> &mut Scary<'a, 'b> { self.data }
-}
-unsafe impl<'e, 'a, 'b> Extract for Forcecast<'e, 'a, 'b> {
+unsafe impl<'e, 'a, 'b> Extract for &'e mut Scary<'a, 'b> {
     fn each_resource(_f: &mut dyn FnMut(TypeId, Access)) {}
-    type Owned = Option<Forcecast<'static, 'static, 'static>>;
+    type Owned = &'e mut Scary<'static, 'static>;
     unsafe fn extract(_universe: &Universe, rez: &mut Rez) -> Self::Owned {
-        Some(Forcecast { data: rez.take_mut_downcast() })
+        rez.take_mut_downcast()
     }
     unsafe fn convert(_universe: &Universe, owned: *mut Self::Owned) -> Self {
-        let owned: Forcecast<'static, 'static, 'static> = (*owned).take().unwrap();
-        // I'm not sure why the transmute is necessary?
+        // FIXME: How sound is this?
+        // I'm primarily worried about multiple mutable aliasing.
+        // It counts as being borrowed...right?
+        let owned: &mut Scary<'static, 'static> = *owned;
+        // What's up with this transmute?
+        // &'static A -> &'a A is fine.
+        // There's some implicit bounds being put on Scary.
         mem::transmute(owned)
     }
     type Cleanup = ();
 }
+
+// ...Okay, but there's still problems here! :|
+// You can extract Scary<'static, 'static>.
 
 #[test]
 fn arg_passing_issue() {
     let universe = Universe::new();
     #[allow(unused_variables, unused_mut)]
     let mut leak: Option<&mut i32> = None;
-    let mut k = Kernel::new(|s: KernelArg<&mut String>, b: Forcecast| {
+    let mut k = Kernel::new(|s: KernelArg<&mut String>, b: &mut Scary| {
         println!("{}", *s);
         println!("{:?}", *b);
         // Uncomment this code to verify that this scheme is (apparently) sound:
-        //leak = Some(b.data.data[0]);
+        //leak = Some(b.data[0]);
     });
     let mut val = format!("hello world!");
     k.push_arg_mut(&mut val);
     let mut n0 = 0;
     let mut n1 = 1;
     let mut data = vec![&mut n0, &mut n1];
-    let data = &mut Scary { data: &mut data };
+    let mut data = Scary { data: &mut data };
     k.push_arg_mut(unsafe { data.forcecast() });
     universe.run(&mut k);
 }
